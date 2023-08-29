49fa3b54873008865ce64368714e4f49a3198d1e
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-29 17:18:58 +0200

    created a question widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  var questions = [
+    'What\'s your favorite color?',
+    'What\'s your favorite animal?',
+  ];
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+      print('Answer chosen!');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,8 +33,22 @@ class MyApp extends StatelessWidget {
             appBar: AppBar(
               title: Text('The App'),
             ),
-            body: Center(
-              child: Text('Hello!'),
+            body: Column(
+              children: [
+                Question(questions[_questionIndex]),
+                ElevatedButton(
+                  child: Text('Answer 1'),
+                  onPressed: _answerQuestion,
+                ),
+                ElevatedButton(
+                  child: Text('Answer 2'),
+                  onPressed: _answerQuestion,
+                ),
+                ElevatedButton(
+                  child: Text('Answer 3'),
+                  onPressed: _answerQuestion,
+                ),
+              ],
             )
         )
     );
